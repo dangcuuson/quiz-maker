75e862e35a549f34dacf38d7e7e8cf3ac6f0d7ab
jack <dangcuuson@gmail.com>
2023-12-22 03:35:41 +1100

    update workspace config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,9 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-type-checked',
+        // Enables eslint-plugin-prettier and eslint-config-prettier.
+        // This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        'plugin:prettier/recommended'
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -10,6 +13,6 @@ module.exports = {
         project: ['./tsconfig.json'],
     },
     plugins: ['@typescript-eslint'],
-    ignorePatterns: [".eslintrc.cjs"],
+    ignorePatterns: [".eslintrc.cjs", "node_modules", ".aws-sam"],
     root: true,
 };
