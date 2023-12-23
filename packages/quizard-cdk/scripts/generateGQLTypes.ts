e639af8785d57b8da95f7bb4f219b89216401c02
jack <dangcuuson@gmail.com>
2023-12-23 23:56:36 +1100

    update graphql-schema-typescript

--- packages/quizard-cdk/scripts/generateGQLTypes.ts
+++ packages/quizard-cdk/scripts/generateGQLTypes.ts
@@ -4,18 +4,17 @@ import { generateTypeScriptTypes } from 'graphql-schema-typescript';
 const SRC_DIR = path.resolve('src/schema');
 const OUTPUT_PATH = path.resolve('src/shared', 'gqlTypes.ts');
 
-generateTypeScriptTypes(SRC_DIR, OUTPUT_PATH, {
-    // typePrefix: '',
-    // contextType: 'GraphQLContext',
-    // importStatements: [
-    //     `import { GraphQLContext } from './index';`
-    // ],
-    customScalarType: {
-        // DateTime: 'Date'
-    }
-})
-.then(() => console.log(`Types generated at ${OUTPUT_PATH}`))
-.catch((err: unknown) =>{
-    console.error(err);
-    process.exit(1);
-});
+generateTypeScriptTypes(
+    SRC_DIR,
+    OUTPUT_PATH,
+    {},
+    {
+        assumeValidSDL: true,
+        assumeValid: true,
+    },
+)
+    .then(() => console.log(`Types generated at ${OUTPUT_PATH}`))
+    .catch((err: unknown) => {
+        console.error(err);
+        process.exit(1);
+    });
