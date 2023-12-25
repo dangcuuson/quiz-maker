8b88c68e931f3d7c1886a005192323f0e9668883
jack <dangcuuson@gmail.com>
2023-12-26 05:22:23 +1100

    fix staging deploy

--- packages/quizard-web/vite.config.ts
+++ packages/quizard-web/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), splitVendorChunkPlugin()],
+    plugins: [react()],
     envDir: path.resolve('./'),
     build: {
         rollupOptions: {
