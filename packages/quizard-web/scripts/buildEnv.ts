a5818001df797cd1634072f7ee09f4384eaa33b3
dangcuuson <dangcuuson@gmail.com>
2023-12-26 18:43:40 +1100

    Local (#13)
    * ignore auto-generated gqlTypes.ts
    
    * update README.md: add dev workflow
    
    * [feat]: add sort by score for ScoresPage with fetchMode='user'

--- packages/quizard-web/scripts/buildEnv.ts
+++ packages/quizard-web/scripts/buildEnv.ts
@@ -1,27 +1,33 @@
 import fs from 'fs';
 import path from 'path';
+import gitBranch from 'git-branch';
 
-const inputFile = path.resolve('../../cdk-outputs.json');
+const start = async () => {
+    const branchName = await gitBranch();
+    const inputFile = path.resolve('../../cdk-outputs.json');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const cdkOutput: { [K: string]: Record<string, string> }
+        = JSON.parse(fs.readFileSync(inputFile, { encoding: 'utf-8' }));
+    const stackName = `quizard-stack-${branchName}`;
+    const jsonData = cdkOutput[stackName];
+    if (!jsonData) {
+        throw Error(
+            `Unable to find ${stackName} in ${inputFile}. Please make sure stack name is correct. If you're checking out to a new branch, ` +
+            `make sure you have npm run deploy the backend stack`
+        );
+    }
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const cdkJsonOutput: { [K: string]: Record<string, string> } = JSON.parse(
-    fs.readFileSync(inputFile, { encoding: 'utf-8' }),
-);
+    const lines = Object.keys(jsonData)
+        .map(key => {
+            return `VITE_${key}=${jsonData[key]}`
+        })
+        .join('\n');
 
-const keys = Object.keys(cdkJsonOutput);
-if (keys.length > 1) {
-    throw Error(`Found multiple stacks`);
+    const outputFile = path.resolve('.env');
+    fs.writeFileSync(outputFile, lines, 'utf-8');
 }
-if (keys.length === 0) {
-    throw Error(`No stack found`);
-}
 
-const jsonData = cdkJsonOutput[keys[0]];
-const lines = Object.keys(jsonData)
-    .map((key) => {
-        return `VITE_${key}=${jsonData[key]}`;
-    })
-    .join('\n');
-
-const outputFile = path.resolve('.env');
-fs.writeFileSync(outputFile, lines, 'utf-8');
+start().catch(err => {
+    console.error(err);
+    process.exit(1);
+})
