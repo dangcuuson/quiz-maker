5fe7f6f362e08d56d727f24e7d55f161ed8ee6fb
jack <dangcuuson@gmail.com>
2024-01-03 07:46:29 +1100

    hooks: update useLocalStorage to store value upon setState callback instead of useEffect

--- packages/quizard-web/src/hooks/hooks.tsx
+++ packages/quizard-web/src/hooks/hooks.tsx
@@ -7,40 +7,36 @@ export function useLocalStorage<T = string>(args: {
     key: string;
     // value stored in localStorage is string, it's up to the hook consumer to
     // convert the value into correct type
-    getInitValue: (value: string | null) => T,
-    stringify?: (value: T) => string
+    getInitValue: (value: string | null) => T;
+    stringify?: (value: T) => string;
 }): [T, React.Dispatch<React.SetStateAction<T>>] {
     const { key, getInitValue, stringify } = args;
-    const [valueState, setValueState] = React.useState<T>(
-        () => {
-            const lsValue = localStorage.getItem(key);
-            return getInitValue(lsValue);
-        }
-    )
-    React.useEffect(
-        () => {
-            const strVal = stringify ? stringify(valueState) : (valueState + '');
-            localStorage.setItem(key, strVal);
-        },
-        [valueState]
-    )
+    const [value, _setValue] = React.useState<T>(() => {
+        const lsValue = localStorage.getItem(key);
+        return getInitValue(lsValue);
+    });
+    const setValue: React.Dispatch<React.SetStateAction<T>> = (newValue) => {
+        const calculatedValue = newValue instanceof Function ? newValue(value) : newValue;
+        const strVal = stringify ? stringify(calculatedValue) : calculatedValue + '';
 
-    return [valueState, setValueState];
+        localStorage.setItem(key, strVal);
+        _setValue(calculatedValue);
+        return calculatedValue;
+    };
+
+    return [value, setValue];
 }
 
 // useEffect with empty dependecy array can still be called twice due to React strict mode
 // this hook will ensure the effectCallback is only called once
 export function useEffectOnce(effectCallback: React.EffectCallback) {
     const calledRef = React.useRef(false);
-    React.useEffect(
-        () => {
-            const called = calledRef.current;
-            if (called) {
-                return;
-            }
-            calledRef.current = true;
-            return effectCallback();
-        },
-        []
-    )
+    React.useEffect(() => {
+        const called = calledRef.current;
+        if (called) {
+            return;
+        }
+        calledRef.current = true;
+        return effectCallback();
+    }, []);
 }
